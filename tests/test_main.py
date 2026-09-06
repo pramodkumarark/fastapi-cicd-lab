5949b29920212d5b9ab7aaf757829ee7802900fc
PramodKumar27 <pramod.kumar@agbeindia.com>
2026-09-06 22:55:40 +0530

    only for CI test

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,7 +10,7 @@ def test_root():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello CI/CD"}
+    assert response.json() == {"message": "Hello CI/CD!"}
 
 
 def test_health():
